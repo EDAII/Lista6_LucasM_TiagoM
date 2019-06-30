5dfb3b5a704981fd3dc439c1a0750b6d47c2ee34
Lucas Medeiros <medeirosrosalucas@gmail.com>
2019-06-30 18:14:22 -0300

    Renderizar tabuleiro
    Co-authored-by: Tiago Miguel <tmcstiago@gmail.com>

--- projeto6.cpp
+++ projeto6.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 int tabuleiro [9][9];
 pair<int, int> jogadas[9][9];
+char rend_tab[9][9];
+const std::string red("\033[0;31m");
+const std::string reset("\033[0m");
 
 void bfs(pair<int, int> pos){
 
 	queue<pair<int, int>> que;
 
 	memset(tabuleiro, -1, sizeof(tabuleiro));
+
+	tabuleiro[pos.first][pos.second] = 0;
 
 	que.push(pos);
 
@@ -19,78 +24,139 @@ void bfs(pair<int, int> pos){
 		pair<int, int> aux;
 		que.pop();
 
-		if(v.first+3 <= 8 and v.second+2 <= 8 and tabuleiro[v.first+3][v.second+2] != -1){
-			tabuleiro[v.first+3][v.second+2] = tabuleiro[v.first][v.second] + 1;
-			jogadas[v.first+3][v.second+2] = v;
-			aux.first = v.first+3;
+		if(v.first+2 <= 8 and v.second+1 <= 8 and tabuleiro[v.first+2][v.second+1] == -1){
+			tabuleiro[v.first+2][v.second+1] = tabuleiro[v.first][v.second] + 1;
+			jogadas[v.first+2][v.second+1] = v;
+			aux.first = v.first+2;
+			aux.second = v.second+1;
+			que.push(aux);
+		}
+
+		if(v.first+2 <= 8 and v.second-1 >= 1 and tabuleiro[v.first+2][v.second-1] == -1){
+			tabuleiro[v.first+2][v.second-1] = tabuleiro[v.first][v.second] + 1;
+			jogadas[v.first+2][v.second-1] = v;
+			aux.first = v.first+2;
+			aux.second = v.second-1;
+			que.push(aux);
+		}
+
+		if(v.first-2 >= 1 and v.second+1 <= 8 and tabuleiro[v.first-2][v.second+1] == -1){
+			tabuleiro[v.first-2][v.second+1] = tabuleiro[v.first][v.second] + 1;
+			jogadas[v.first-2][v.second+1] = v;
+			aux.first = v.first-2;
+			aux.second = v.second+1;
+			que.push(aux);
+		}
+
+		if(v.first-2 >= 1 and v.second-1 >= 1 and tabuleiro[v.first-2][v.second-1] == -1){
+			tabuleiro[v.first-2][v.second-1] = tabuleiro[v.first][v.second] + 1;
+			jogadas[v.first-2][v.second-1] = v;
+			aux.first = v.first-2;
+			aux.second = v.second-1;
+			que.push(aux);
+		}
+
+		if(v.first+1 <= 8 and v.second+2 <= 8 and tabuleiro[v.first+1][v.second+2] == -1){
+			tabuleiro[v.first+1][v.second+2] = tabuleiro[v.first][v.second] + 1;
+			jogadas[v.first+1][v.second+2] = v;
+			aux.first = v.first+1;
 			aux.second = v.second+2;
 			que.push(aux);
 		}
 
-		if(v.first+3 <= 8 and v.second-2 >= 1 and tabuleiro[v.first+3][v.second-2] != -1){
-			tabuleiro[v.first+3][v.second-2] = tabuleiro[v.first][v.second] + 1;
-			jogadas[v.first+3][v.second-2] = v;
-			aux.first = v.first+3;
+		if(v.first+1 <= 8 and v.second-2 >= 1 and tabuleiro[v.first+1][v.second-2] == -1){
+			tabuleiro[v.first+1][v.second-2] = tabuleiro[v.first][v.second] + 1;
+			jogadas[v.first+1][v.second-2] = v;
+			aux.first = v.first+1;
 			aux.second = v.second-2;
 			que.push(aux);
 		}
 
-		if(v.first-3 >= 1 and v.second+2 <= 8 and tabuleiro[v.first-3][v.second+2] != -1){
-			tabuleiro[v.first-3][v.second+2] = tabuleiro[v.first][v.second] + 1;
-			jogadas[v.first-3][v.second+2] = v;
-			aux.first = v.first-3;
+		if(v.first-1 >= 1 and v.second+2 <= 8 and tabuleiro[v.first-1][v.second+2] == -1){
+			tabuleiro[v.first-1][v.second+2] = tabuleiro[v.first][v.second] + 1;
+			jogadas[v.first-1][v.second+2] = v;
+			aux.first = v.first-1;
 			aux.second = v.second+2;
 			que.push(aux);
 		}
 
-		if(v.first-3 >= 1 and v.second-2 >= 1 and tabuleiro[v.first-3][v.second-2] != -1){
-			tabuleiro[v.first-3][v.second-2] = tabuleiro[v.first][v.second] + 1;
-			jogadas[v.first-3][v.second-2] = v;
-			aux.first = v.first-3;
+		if(v.first-1 >= 1 and v.second-2 >= 1 and tabuleiro[v.first-1][v.second-2] == -1){
+			tabuleiro[v.first-1][v.second-2] = tabuleiro[v.first][v.second] + 1;
+			jogadas[v.first-1][v.second-2] = v;
+			aux.first = v.first-1;
 			aux.second = v.second-2;
-			que.push(aux);
-		}
-
-		if(v.first+2 <= 8 and v.second+3 <= 8 and tabuleiro[v.first+2][v.second+3] != -1){
-			tabuleiro[v.first+2][v.second+3] = tabuleiro[v.first][v.second] + 1;
-			jogadas[v.first+2][v.second+3] = v;
-			aux.first = v.first+2;
-			aux.second = v.second+3;
-			que.push(aux);
-		}
-
-		if(v.first+2 <= 8 and v.second-3 >= 1 and tabuleiro[v.first+2][v.second-3] != -1){
-			tabuleiro[v.first+2][v.second-3] = tabuleiro[v.first][v.second] + 1;
-			jogadas[v.first+2][v.second-3] = v;
-			aux.first = v.first+2;
-			aux.second = v.second-3;
-			que.push(aux);
-		}
-
-		if(v.first-2 >= 1 and v.second+3 <= 8 and tabuleiro[v.first-2][v.second+3] != -1){
-			tabuleiro[v.first-2][v.second+3] = tabuleiro[v.first][v.second] + 1;
-			jogadas[v.first-2][v.second+3] = v;
-			aux.first = v.first-2;
-			aux.second = v.second+3;
-			que.push(aux);
-		}
-
-		if(v.first-2 >= 1 and v.second-3 >= 1 and tabuleiro[v.first-2][v.second-3] != -1){
-			tabuleiro[v.first-2][v.second-3] = tabuleiro[v.first][v.second] + 1;
-			jogadas[v.first-2][v.second-3] = v;
-			aux.first = v.first-2;
-			aux.second = v.second-3;
 			que.push(aux);
 		}
 	}
 }
 
+void reset_tabuleiro() {
+
+	for(int i=1, k=0; i<9; i++){
+		for(int j=1; j<9; j++, k++){
+			if(k%2 == 0){
+				rend_tab[i][j] = 'o';
+			}else{
+				rend_tab[i][j] = '#';
+			}
+		}
+		k++;
+	}
+
+}
+
+void print_tab(){
+
+	cout << "  1 2 3 4 5 6 7 8" << endl;
+
+	for(int i=1; i<9; i++){
+		cout << i << " ";
+		for(int j=1; j<9; j++){
+			if(rend_tab[i][j] != '#' && rend_tab[i][j] != 'o'){
+				cout << red << rend_tab[i][j] << reset <<" ";
+			}else {
+				cout << rend_tab[i][j] << " ";
+			}
+		}
+		cout << endl;
+	}
+
+}
+
+void moves(pair<int, int> end){
+
+	while(tabuleiro[end.first][end.second] != 0){
+		rend_tab[end.first][end.second] = (char) (tabuleiro[end.first][end.second] + '0');
+		end = jogadas[end.first][end.second];
+	}	
+
+}
+
 int main() {
 
-	pair<int, int> in;
+	pair<int, int> in, out;
 
+	while(1){
+	
+		cin >> in.first >> in.second;
 
+		reset_tabuleiro();
 
+		rend_tab[in.first][in.second] = 's';
+
+		bfs(in);
+
+		cin >> out.first >> out.second;
+
+		moves(out);
+
+		print_tab();
+
+		cout << "Distância até o ponto selecionado: " << tabuleiro[out.first][out.second] << endl;
+
+	}
+
+	print_tab();
 
 	return 0;
 }
